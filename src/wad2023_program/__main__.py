0fb9343f4841b06e410555ae9b4ea007c25835db
Daryl Stark <daryl@dstark.nl>
2023-07-07 15:21:56 +0200

    Placed the view in a seperate module

--- src/wad2023_program/__main__.py
+++ src/wad2023_program/__main__.py
@@ -6,10 +6,6 @@ Contains the CLI script for the `wad23` application.
 from enum import Enum
 from logging import INFO, basicConfig, getLogger
 
-from rich import box
-from rich.console import Console, Group
-from rich.panel import Panel
-from rich.table import Table
 from sqlmodel import Session as DBSession
 from sqlmodel import or_, select
 from typer import Typer
@@ -18,8 +14,10 @@ from wad2023_program.sync import sync_sessions, sync_speakers
 
 from .app_config import AppConfig
 from .database import create_tables, get_db_engine
-from .model import Session, SessionType, Speaker
+from .model import Session, SessionType
 from .sessionize_parser import SessionizeParser
+from .view import (view_sessions_as_csv, view_sessions_as_details,
+                   view_sessions_as_table)
 
 # Create the Typer app
 app = Typer(name='WeAreDevelopers 2023 Conference')
@@ -54,28 +52,6 @@ class OutputType(str, Enum):
     TABLE = 'table'
     CSV = 'csv'
     DETAILS = 'details'
-
-
-def speaker_text(speaker: Speaker) -> str:
-    """Create a text for a speaker.
-
-    Returns a string in a default format for a speaker.
-
-    Args:
-        speaker: the speaker object.
-
-    Returns:
-        The format for the speaker.
-    """
-    output_text = f'\n[yellow][b]{speaker.name}[/b][/yellow]'
-
-    if len(speaker.tagline) > 0:
-        output_text += f'\n[orange][i]{speaker.tagline}[/i][/orange]'
-
-    if len(speaker.bio) > 0:
-        output_text += '\n\n' + speaker.bio
-
-    return output_text
 
 
 @app.command(name='sync', help='Synchronize the database')
@@ -183,70 +159,13 @@ def list_sessions(
         all_sessions = session.exec(statement).all()
 
         if output == OutputType.TABLE:
-            # Display the sessions
-            console = Console()
-            table = Table(box=box.HORIZONTALS)
-            table.add_column('*')
-            table.add_column('Type')
-            table.add_column('Date')
-            table.add_column('Start')
-            table.add_column('End')
-            table.add_column('Stage')
-            table.add_column('Title')
-            table.add_column('Speakers')
-            for sess in all_sessions:
-                table.add_row(
-                    '*' if sess.favourite else '',
-                    sess.session_type.capitalize(),
-                    f'{sess.start_time_berlin:%Y-%m-%d}',
-                    f'{sess.start_time_berlin:%H:%M}',
-                    f'{sess.end_time_berlin:%H:%M}',
-                    sess.stage.name,
-                    sess.title,
-                    ', '.join([speaker.name for speaker in sess.speakers])
-                )
-            console.print(table)
+            view_sessions_as_table(all_sessions)
 
         if output == OutputType.CSV:
-            columns = ('Favourite', 'Type', 'Date', 'Start',
-                       'End', 'Stage', 'Title', 'Speakers')
-            print(';'.join([f'"{column}"' for column in columns]))
-            for sess in all_sessions:
-                row = ['*' if sess.favourite else '',
-                       sess.session_type.capitalize(),
-                       f'{sess.start_time_berlin:%Y-%m-%d}',
-                       f'{sess.start_time_berlin:%H:%M}',
-                       f'{sess.end_time_berlin:%H:%M}',
-                       sess.stage.name,
-                       sess.title,
-                       ', '.join([speaker.name for speaker in sess.speakers])]
-                print(';'.join([f'"{row_data}"' for row_data in row]))
+            view_sessions_as_csv(all_sessions)
 
         if output == OutputType.DETAILS:
-            console = Console()
-            for sess in all_sessions:
-                table = Table(box=box.MINIMAL, show_header=False)
-                table.add_column('Field')
-                table.add_column('Information')
-                table.add_row('Title', sess.title)
-                table.add_row('Session ID', str(sess.id))
-                table.add_row(
-                    'Date', (f'{sess.start_time_berlin:%Y-%m-%d} ' +
-                             f'({sess.start_time_berlin:%H:%M} - ' +
-                             f'{sess.end_time_berlin:%H:%M})'))
-                table.add_row('Stage', sess.stage.name)
-
-                output_parts: list[str | Table] = [
-                    table, sess.description,
-                    '\n[green][b]## Speakers[/b][/green]']
-
-                for speaker in sess.speakers:
-                    output_parts.append(speaker_text(speaker))
-
-                console.print(
-                    Panel(
-                        Group(*output_parts)
-                    ))
+            view_sessions_as_details(all_sessions)
 
 
 if __name__ == '__main__':
